4b9154d6f326e049ae8c3d40db446150f414ac59
infinixs5lite <kelvinchinedu97@gmail.com>
2023-03-02 19:34:35 +0100

    Update twrp_X698.mk

--- twrp_X698.mk
+++ twrp_X698.mk
@@ -9,17 +9,21 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from Infinix-X698 device
+# Inherit from X698 device
 $(call inherit-product, device/infinix/X698/device.mk)
 
 PRODUCT_DEVICE := X698
 PRODUCT_NAME := twrp_X698
-PRODUCT_BRAND := Infinix
+PRODUCT_BRAND := Ifinix
 PRODUCT_MODEL := Infinix X698
 PRODUCT_MANUFACTURER := infinix
+PRODUCT_RELEASE_NAME := infinix note 11s
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
 
